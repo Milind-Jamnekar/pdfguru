f3bfa860953aa4da7faf9e290c3701707461cd8d
Milind Jamnekar <milindjamnekar502@gmail.com>
2023-10-10 23:12:46 +0530

    renderer done

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -1,41 +1,42 @@
 import ChatWrapper from "@/components/ChatWrapper";
 import PdfRenderer from "@/components/PdfRenderer";
 import { db } from "@/db";
-import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
 
-type PageProps = {
+interface PageProps {
   params: {
     fileId: string;
   };
-};
+}
 
 const Page = async ({ params: { fileId } }: PageProps) => {
   const { getUser } = getKindeServerSession();
   const user = getUser();
 
-  if (!user || !user.id) redirect(`/auth-callback/origin=dashboard/${fileId}`);
+  if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileId}`);
 
   const file = await db.file.findFirst({
-    where: { id: fileId, userId: user.id },
+    where: {
+      id: fileId,
+      userId: user.id,
+    },
   });
 
   if (!file) notFound();
+
   return (
-    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-4.5rem)]">
-      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2 ">
-        {/* left side  */}
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        {/* Left sidebar & main wrapper */}
         <div className="flex-1 xl:flex">
-          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:px-6">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            {/* Main area */}
             <PdfRenderer file={file.url} />
           </div>
         </div>
 
-        <div
-          className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96
-        lg:border-1 lg:border-t-0 bg-red-300"
-        >
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
           <ChatWrapper />
         </div>
       </div>
