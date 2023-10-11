4020cf8824f5a599be3e5582da648871291d708b
Milind Jamnekar <milindjamnekar502@gmail.com>
2023-10-11 10:44:59 +0530

    some ui tweaks

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -25,7 +25,7 @@ const Dashboard = () => {
   return (
     <main className="mx-auto max-w-7xl md:p-10">
       <div
-        className="mt-8 flex flex-col items-start justify-between 
+        className="mt-8 mx-5 md:mx-10 flex flex-col items-start justify-between 
       gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0"
       >
         <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
@@ -33,7 +33,7 @@ const Dashboard = () => {
       </div>
 
       {files && files?.length !== 0 ? (
-        <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3">
+        <ul className="mt-8 mx-5 md:mx-10 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3">
           {files
             .sort(
               (a, b) =>
