0ce05e4e94ea0a984dbdf5dabc22ba0a870a6836
Milind Jamnekar <milindjamnekar502@gmail.com>
2023-10-11 10:37:24 +0530

    bug removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import Navbar from "@/components/Navbar";
 import Provider from "@/app/_trpc/Provider";
 
 import "react-loading-skeleton/dist/skeleton.css";
+import "simplebar-react/dist/simplebar.min.css";
+
 import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
